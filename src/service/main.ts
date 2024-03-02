a3222b3b500f835f1b76ab539c3bbc52bbd06d29
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-03-02 13:38:27 +0800

    feat: add enable for cors api

--- src/service/main.ts
+++ src/service/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all routes
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
