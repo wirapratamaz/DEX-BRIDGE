9c285db0131962ba27f09d2b05d4cd360bcd3461
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-03-02 14:56:13 +0800

    feat: add interface response and create service handle operation to an exchange

---
+++ src/exchange/interfaces/interface.response.ts
@@ -0,0 +1,23 @@
+export interface OrderResponse {
+  status: string;
+  response: OrderResponse | CancelResponse;
+}
+export interface OrderResponse {
+  type: 'order';
+  data: {
+    statuses: OrderStatus[];
+  };
+}
+
+export interface OrderStatus {
+  resting: {
+    oid: number;
+  };
+}
+
+export interface CancelResponse {
+  type: 'cancel';
+  data: {
+    statuses: string[];
+  };
+}
